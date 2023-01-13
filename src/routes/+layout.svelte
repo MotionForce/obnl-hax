b0ddab98d3d0cb9f4e69ad95283aabe1765e90dc
MotionForce <motionforce@outlook.com>
2023-01-13 17:38:21 -0500

    added footer and fixed +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,27 @@
 <script>
     import Navbar from "$lib/navbar.svelte";
+
+    let year = new Date().getFullYear();
 </script>
 
 <main>
-    <Navbar />
+    <Navbar/>
+    <slot/>
+    <footer>
+        <p>Copyright {year}</p>
+    </footer>
 </main>
 
 <style>
     * {
         font-family: sans-serif;
+    }
+
+    footer {
+        bottom: 0;
+        left: 50%;
+        text-align: center;
+        margin-top: 2em;
+
     }
 </style>
