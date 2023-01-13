2a6ff3dad7d9d588219e64da72eae8afd6f110e4
MotionForce <motionforce@outlook.com>
2023-01-13 17:20:46 -0500

    changed home page to talk about WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to WIP</h1>
+<p>Visit <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">github.com</a> to read the documentation</p>
