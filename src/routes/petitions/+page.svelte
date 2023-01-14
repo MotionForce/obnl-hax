169091897e2b1248a1879341f5c9510c7a56bafc
MotionForce <motionforce@outlook.com>
2023-01-13 19:32:42 -0500

    added margins and basic element placement to petitions/+page.svelte

--- src/routes/petitions/+page.svelte
+++ src/routes/petitions/+page.svelte
@@ -5,12 +5,13 @@
         {
             id: 1,
             title: "Petition 1",
-            description: "Petition 1 description",
+            user: "user1",
         },
         {
             id: 2,
             title: "Petition 2",
-            description: "Petition 2 description"}];
+            user: "user2",
+        }];
 </script>
 
 <div class="petitions">
@@ -18,7 +19,7 @@
     {#each petitions as petition}
         <div class="petition">
             <h2>{petition.title}</h2>
-            <p>{petition.description}</p>
+            <p>{petition.user}</p>
             <a href="{petition.url}">Sign</a>
         </div>
     {/each}
@@ -32,11 +33,32 @@
     }
 
     .petition {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        display: inline-grid; /*align-items: center;*/
         border: 1px solid black;
-        width: 50%;
+        width: 100%;
         margin: 10px;
+    }
+
+    h2 {
+        margin: 3px auto 0 15px;
+        float: left;
+        grid-column: 1;
+        grid-row: 1;
+    }
+
+    a {
+        grid-column: 3;
+        grid-row-start: 1;
+        grid-row-end: 2;
+        margin-right: 30px;
+        margin-left: auto;
+        padding-top: 5%;
+
+    }
+
+    p {
+        margin: 0 auto 3px 15px;
+        grid-column: 1;
+        grid-row: 2;
     }
 </style>
