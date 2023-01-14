ebe8d7cb6774e4df7f639e3042395b28bba3f15a
MotionForce <motionforce@outlook.com>
2023-01-14 12:30:21 -0500

    added ui for the 4th time :)

--- src/routes/petitions/[id]/+page.svelte
+++ src/routes/petitions/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {pb} from "$lib/auth.js"
-    import { construct_svelte_component, identity } from "svelte/internal";
+    import {construct_svelte_component, identity} from "svelte/internal";
 
     export let data
     let petition = {
@@ -9,10 +9,10 @@
         creator: "",
         signers: JSON.parse("[]"),
     }
-    let signed = false
-    let failed_fetch = false
-    let failed_sign = false
-    let already_signed = false
+    let signed = false;
+    let failed_fetch = false;
+    let failed_sign = false;
+    let already_signed = false;
 
     async function fetch_petition() {
         try {
@@ -27,14 +27,13 @@
             failed_fetch = true;
         }
     }
-    
-    async function sign(){
-        try{
-            if (petition.signers.some(item => item === pb.authStore.model.name)){
+
+    async function sign() {
+        try {
+            if (petition.signers.some(item => item === pb.authStore.model.name)) {
                 already_signed = true
                 return
             }
-            petition.signers.push(pb.authStore.model.name)
             await pb.collection("petitions").update(`${data.post.id}`, {
                 title: petition.title,
                 content: petition.content,
@@ -50,16 +49,104 @@
     fetch_petition()
 
 </script>
-<h2>{petition.title}</h2>
-<br>
-<p>{petition.content}</p>
-Signers<br>
-<ul>
-{#each petition.signers as signer}
-<li>{signer}</li>
-{/each}
-</ul>
-<button on:click={fetch_petition}>Rafraichir</button>
-{#if !signed}
-<button on:click={sign}>sign</button>
-{/if}
+
+<div className="petition">
+    <div className="title">
+        <div className="title-h1-group">
+            <h1>{petition.title}</h1>
+            {#if !signed}
+                <button on:click={sign}>Signer</button>
+            {/if}
+        </div>
+        <p>Created by {petition.creator}</p>
+    </div>
+    <div class="content">
+        <p>{petition.content}</p>
+    </div>
+    <div class="signers">
+        <h6>Signers: </h6>
+        <ul>
+            {#each petition.signers as signer}
+                <li><p>{signer}</p></li>
+            {/each}
+        </ul>
+    </div>
+    <div class="buttons">
+        <button on:click={fetch_petition}>Rafraichir</button>
+        {#if !signed}
+            <button on:click={sign}>Signer</button>
+        {/if}
+    </div>
+    <div>
+        {#if failed_fetch}
+            <p>Failed to fetch petition</p>
+        {/if}
+        {#if failed_sign}
+            <p>Failed to sign petition</p>
+        {/if}
+        {#if already_signed}
+            <p>You already signed this petition</p>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .petition {
+        display: flex;
+        flex-direction: column;
+        background-color: #3bb43e;
+        width: 80%;
+        padding: 40px;
+        border-radius: 15px;
+        text-align: justify;
+        text-justify: inter-word;
+        border: black 1px solid;
+        box-shadow: black 10px 10px;
+        margin: 50px 50px 50px 11%;
+        transform: translateX(-5%);
+    }
+
+    .title-h1-group {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .title h1 {
+        font-size: 4rem;
+        margin-bottom: 0;
+        margin-top: 5px;
+    }
+
+    .title p {
+        font-size: 1.5rem;
+        margin-bottom: 15px;
+        margin-top: 0;
+    }
+
+    .signers {
+        display: flex;
+        flex-direction: column;
+        margin-top: 25px;
+    }
+
+    .signers h6 {
+        font-size: 1.5rem;
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+
+    .signers p {
+        font-size: 1.25rem;
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+
+    button {
+        margin-top: 25px;
+        margin-bottom: 25px;
+        margin-right: 20px;
+        padding: 10px;
+        width: 10%;
+    }
+</style>
