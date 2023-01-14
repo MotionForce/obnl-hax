2a9b0b900fe00e6be4dc9d57bd28a45a4cf7ca23
MotionForce <motionforce@outlook.com>
2023-01-14 15:54:53 -0500

    added framework for localization of text and home page localization

---
+++ src/lib/translation.js
@@ -0,0 +1,36 @@
+export default {
+    en: {
+        main: {
+            title: 'Petition Manager',
+            connect: 'Login',
+            viewPetitions: 'View petitions',
+        },
+        navbar: {
+            language: {
+                en: 'English',
+                fr: 'Français',
+            },
+            blog: 'Blog',
+            petitions: 'Petitions',
+            logout: 'Logout',
+            login: 'Login',
+        },
+    },
+    fr: {
+        main: {
+            title: 'Gestionnaire Pétition',
+            connect: 'Se connecter',
+            viewPetitions: 'Voir les pétition',
+        },
+        navbar: {
+            language: {
+                en: 'Anglais',
+                fr: 'Français',
+            },
+            blog: 'Blog',
+            petitions: 'Pétitions',
+            logout: 'Se déconnecter',
+            login: 'Se connecter',
+        }
+    },
+};
